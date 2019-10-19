99ee6661edd2223953c019f9e882eb5f5e7f4e28
Morgan <morganichoui@gmail.com>
2019-10-19 20:14:51 +0200

    Input fonctionnent, style ion-page

--- src/app/tab3/tab3.module.ts
+++ src/app/tab3/tab3.module.ts
@@ -4,13 +4,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab3Page } from './tab3.page';
+import { HeaderPageModule } from '../header/header.module';
 
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
-    RouterModule.forChild([{ path: '', component: Tab3Page }])
+    RouterModule.forChild([{path: '', component: Tab3Page}]),
+    HeaderPageModule
   ],
   declarations: [Tab3Page]
 })
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -7,6 +7,7 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
+  pageName = 'Contacts';
   constructor() {}
 
 }
