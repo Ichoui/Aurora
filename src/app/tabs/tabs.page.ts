245f144e4a8cc16c92a5c63b3891d9839e3b86b5
Morgan <morganichoui@gmail.com>
2019-10-19 17:52:40 +0200

    résolution pbm du btn central

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,16 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
-  selector: 'app-tabs',
-  templateUrl: 'tabs.page.html',
-  styleUrls: ['tabs.page.scss']
+    selector: 'app-tabs',
+    templateUrl: 'tabs.page.html',
+    styleUrls: ['tabs.page.scss']
 })
 export class TabsPage implements OnInit {
 
-  constructor() {}
+    @ViewChild('test', {static: false}) test;
 
-  ngOnInit(): void {
-  }
+    constructor() {
+    }
+
+    ngOnInit(): void {
+        this.interval();
+    }
+
+
+    /*
+    * Permet d'afficher le logo Aurora
+    * tag <a> est dans shadowRoot et ne se charge pas immédiatement
+    * Interval enchaîne les itérations jusqu'à ce qu'il existe et s'arrête
+    * */
+    interval(): void {
+        let inter = setInterval(() => {
+            let fuckingA = this.test.el.shadowRoot.querySelector('a'); // null
+            if (fuckingA !== null) {
+                fuckingA.style.overflow = 'visible';
+                clearInterval(inter);
+            }
+        }, 50);
+    }
 
 
 }
