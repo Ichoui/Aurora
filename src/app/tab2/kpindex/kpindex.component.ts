c18528098b09ba4b7196d38d744708218c348426
Morgan Nicol <morganichoui@gmail.com>
2019-11-08 18:14:13 +0100

    Meteo side

--- src/app/tab2/kpindex/kpindex.component.ts
+++ src/app/tab2/kpindex/kpindex.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Coords } from '../../cities';
 
 @Component({
   selector: 'app-kpindex',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./kpindex.component.scss'],
 })
 export class KpindexComponent implements OnInit {
+
+  @Input() coords: Coords;
 
   constructor() { }
 
