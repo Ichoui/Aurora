55c61be754b11ee943bcc28da80346036272aef0
Morgan <morganichoui@gmail.com>
2019-11-17 23:41:55 +0100

    Notifications, ça pète la cervelle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -28,7 +28,6 @@ export class AppComponent {
         private statusBar: StatusBar,
         private oneSignal: OneSignal,
         private alertCtrl: AlertController
-
     ) {
         this.initializeApp();
         translateService.setDefaultLang('fr');
@@ -41,19 +40,26 @@ export class AppComponent {
             this.splashScreen.hide();
             this.getKp();
             this.isNotifsActive();
-            //
-            // if (this.platform.is('cordova')) {
-            //     this.setupPush();
-            // }
 
-            // const notificationOpenedCallback = function(jsonData) {
+            if (this.platform.is('cordova')) {
+                this.setupPush();
+            }
+
+            // const notificationOpenedCallback = function (jsonData) {
+            //     console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
+            // };
+            // const notificationReceivedCallback = function (jsonData) {
             //     console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
             // };
             //
-            // window["plugins"].OneSignal
+            // window['plugins'].OneSignal
             //     .startInit(ONESIGNAL_APP_ID, GOOGLE_PROJECT_NUMBER)
             //     .handleNotificationOpened(notificationOpenedCallback)
+            //     .handleNotificationReceived(notificationReceivedCallback)
             //     .endInit();
+
+            // soit c'est l'un soit c'est l'autre
+
         });
     }
 
@@ -85,44 +91,49 @@ export class AppComponent {
 
 
 
-   // setupPush() {
-   //      // I recommend to put these into your environment.ts
-   //      this.oneSignal.startInit(ONESIGNAL_APP_ID, GOOGLE_PROJECT_NUMBER);
-   //
-   //      this.oneSignal.inFocusDisplaying(this.oneSignal.OSInFocusDisplayOption.None);
-   //
-   //      // Notifcation was received in general
-   //      this.oneSignal.handleNotificationReceived().subscribe(data => {
-   //          let msg = data.payload.body;
-   //          let title = data.payload.title;
-   //          let additionalData = data.payload.additionalData;
-   //          this.showAlert(title, msg, additionalData.task);
-   //      });
-   //
-   //      // Notification was really clicked/opened
-   //      this.oneSignal.handleNotificationOpened().subscribe(data => {
-   //          // Just a note that the data is a different place here!
-   //          let additionalData = data.notification.payload.additionalData;
-   //
-   //          this.showAlert('Notification opened', 'You already read this before', additionalData.task);
-   //      });
-   //
-   //      this.oneSignal.endInit();
-   //  }
-   //
-   //  async showAlert(title, msg, task) {
-   //      const alert = await this.alertCtrl.create({
-   //          header: title,
-   //          subHeader: msg,
-   //          buttons: [
-   //              {
-   //                  text: `Action: ${task}`,
-   //                  handler: () => {
-   //                      // E.g: Navigate to a specific screen
-   //                  }
-   //              }
-   //          ]
-   //      });
-   //      await alert.present();
-   //  }
+    setupPush() {
+        // //I recommend to put these into your environment.ts
+            this.oneSignal.startInit(ONESIGNAL_APP_ID, GOOGLE_PROJECT_NUMBER);
+
+        this.oneSignal.inFocusDisplaying(this.oneSignal.OSInFocusDisplayOption.Notification);
+
+            // Notifcation was received in general
+            this.oneSignal.handleNotificationReceived().subscribe(data => {
+                console.log(data);
+                let msg = data.payload.body;
+                let title = data.payload.title;
+                // let additionalData = data.payload.additionalData;
+                // console.log(additionalData);
+                this.showAlert(title, msg);
+            });
+        console.log(this.oneSignal);
+
+            // Notification was really clicked/opened
+            this.oneSignal.handleNotificationOpened().subscribe(data => {
+                // Just a note that the data is a different place here!
+                let additionalData = data.notification.payload.additionalData;
+
+                this.showAlert('Notification opened', 'You already read this before', additionalData.task);
+            });
+
+            this.oneSignal.endInit();
+        }
+
+        async showAlert(title, msg, task?) {
+        console.log('?');
+            const alert = await this.alertCtrl.create({
+                header: title,
+                subHeader: msg,
+                buttons: [
+                    {
+                        text: `OK`,
+                        handler: e => {
+                            console.log(e);
+                            // E.g: Navigate to a specific screen
+                        }
+                    }
+                ]
+            });
+            alert.present();
+    }
 }
