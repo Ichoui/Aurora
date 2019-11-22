a3abe744a7e178c8041b05dd4e5cfa730de3e618
Morgan Nicol <morganichoui@gmail.com>
2019-11-22 15:40:26 +0100

    OUPS.

--- src/app/tab2/maps/maps.component.ts
+++ src/app/tab2/maps/maps.component.ts
@@ -1,7 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { ModalComponent } from '../../shared/modal/modal.component';
 import { TranslateService } from '@ngx-translate/core';
+import * as Chart from 'chart.js';
+import ChartDataLabels from 'chartjs-plugin-datalabels';
+import { Kp27day } from '../../models/aurorav2';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
     selector: 'app-maps',
@@ -10,10 +14,21 @@ import { TranslateService } from '@ngx-translate/core';
 })
 export class MapsComponent implements OnInit {
 
+    kpForecast$ = new BehaviorSubject<Kp27day>(null);
+
+    @Input()
+    set kpForecastInput(value: Kp27day) {
+        this.kpForecast$.next(value);
+    }
+    get kpForecastInput() {
+        return this.kpForecast$.getValue();
+    }
+
     constructor(private modalController: ModalController, private translateService: TranslateService ) {
     }
 
     ngOnInit() {
+        this.chartForecast();
     }
 
     async showMap() {
@@ -38,5 +53,100 @@ export class MapsComponent implements OnInit {
       }
     });
     return await modal.present();
+  }
+
+  chartForecast() {
+        this.kpForecast$.subscribe(
+            res => {
+                console.log(res);
+            }
+        );
+        // 14 values
+      new Chart('kpforecast', {
+          type: 'bar',
+          plugins: [ChartDataLabels],
+          data: {
+              labels: ['a','b','c','d','a','b','c','d','a','b','c','d','a','b'],
+              datasets: [{
+                  data: [1,5,3,8,9,5,4,6,1,5,3,8,9,50],
+                  backgroundColor: [
+                      'rgba(105, 191, 175, 0.4)',
+                      'rgba(105, 191, 175, .8)',
+                      'rgba(105, 191, 175, 0.4)',
+                      'rgba(105, 191, 175, .8)',
+                      'rgba(105, 191, 175, 0.4)',
+                      'rgba(105, 191, 175, .8)',
+                      'rgba(105, 191, 175, 0.4)',
+                      'rgba(105, 191, 175, .8)',
+                      'rgba(105, 191, 175, 0.4)',
+                      'rgba(105, 191, 175, .8)',
+                      'rgba(105, 191, 175, 0.4)',
+                      'rgba(105, 191, 175, .8)',
+                      'rgba(105, 191, 175, 0.4)',
+                      'rgba(105, 191, 175, .8)'
+                  ],
+                  borderColor: [
+                      'rgba(105, 191, 175, 0.4)',
+                      'rgba(105, 191, 175, .8)',
+                      'rgba(105, 191, 175, 0.4)',
+                      'rgba(105, 191, 175, .8)',
+                      'rgba(105, 191, 175, 0.4)',
+                      'rgba(105, 191, 175, .8)',
+                      'rgba(105, 191, 175, 0.4)',
+                      'rgba(105, 191, 175, .8)',
+                      'rgba(105, 191, 175, 0.4)',
+                      'rgba(105, 191, 175, .8)',
+                      'rgba(105, 191, 175, 0.4)',
+                      'rgba(105, 191, 175, .8)',
+                      'rgba(105, 191, 175, 0.4)',
+                      'rgba(105, 191, 175, .8)'
+                  ],
+                  borderWidth: 2,
+              }]
+          },
+          options: {
+              responsive: true,
+              plugins: {
+                  datalabels: {
+                      anchor: 'end',
+                      align: 'end',
+                      color: '#8cffea',
+                      font: {
+                          family: 'Oswald-SemiBold',
+                          size: 15
+                      }
+                  }
+              },
+              legend: {
+                  display: false
+              },
+              scales: {
+                  xAxes: [{
+                      gridLines: {
+                          display: false
+                      },
+                      ticks: {
+                          fontColor: '#949494',
+                          fontFamily: 'Oswald-SemiBold'
+                      }
+                  }],
+                  yAxes: [{
+                      display: false,
+                      gridLines: {
+                          display: false
+                      }
+                  }]
+              },
+              layout: {
+                  padding: {
+                      top: 30
+                  }
+              },
+              tooltips: {
+                  enabled: false
+              }
+          }
+      });
+
   }
 }
