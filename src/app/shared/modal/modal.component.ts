b0629c08cae3762f96c2f6ed9301492af88ac8cc
Morgan Nicol <morganichoui@gmail.com>
2019-12-05 14:45:16 +0100

    Hour / Date time ion range

--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -8,6 +8,11 @@ export interface Ovations {
     url: string;
 }
 
+export interface DateTimes {
+    hour: string;
+    date: string;
+}
+
 @Component({
     selector: 'app-modal',
     templateUrl: './modal.component.html',
@@ -27,12 +32,14 @@ export class ModalComponent implements OnInit {
     @Input() cgu: boolean = false;
     @Input() canvasInput: boolean = false;
 
-    canvasModal: string = 'none'; //unused : canvas style
-
     tabNorth = [];
     minNorth: number = 0;
     maxNorth: number = 0;
     valueNorth: number = 0;
+    datetimeNorth = [];
+    HourNorth: string = '00:00';
+    DateNorth: string = '01/01';
+
 
     tabSouth = [];
     minSouth: number = 0;
@@ -59,6 +66,11 @@ export class ModalComponent implements OnInit {
 
     valueOvationsChange(e, pole: string) {
         pole === 'north' ? this.valueNorth = e.detail.value : this.valueSouth = e.detail.value;
+        // décalage de 5 minutes, faut p'tet gérer un truc en plus :) mais marche
+        if (this.datetimeNorth.length > 0 && pole === 'north') {
+            this.HourNorth = this.datetimeNorth[e.detail.value][0];
+            this.DateNorth = this.datetimeNorth[e.detail.value][1];
+        }
     }
 
     loadOvationNorth() {
@@ -69,15 +81,16 @@ export class ModalComponent implements OnInit {
                 resp.forEach(snapshot => this.tabNorth.push(prefixUrl + snapshot.url));
                 console.log(this.tabNorth[10]);
                 this.tabNorth.forEach(e => {
-                    const segment = e.split('/');
-                    console.log(segment);
-                    console.log(segment.split('_'));
-                    // const segmentUrl = segment[9];
-                    // console.log(segmentUrl);
-                    // const date = segmentUrl.split('');
-                    // console.log(date);
-
-                })
+                    const fullUrl = e.split('/');
+                    const segmentUrl = fullUrl[9];
+                    if (segmentUrl) {
+                        const segment_segmented = segmentUrl.split('_');
+                        const date = segment_segmented[4];
+                        const hourNotFormatted = segment_segmented[5];
+                        const hour = hourNotFormatted.split('.');
+                        this.formattedData(date, hour[0], 'north');
+                    }
+                });
             }
         );
     }
@@ -88,8 +101,24 @@ export class ModalComponent implements OnInit {
                 this.maxSouth = resp.length;
                 const prefixUrl = 'https://services.swpc.noaa.gov/';
                 resp.forEach(snapshot => this.tabSouth.push(prefixUrl + snapshot.url));
-                console.log(this.tabSouth[10]);
             }
         );
+    }
+
+    formattedData(date?: string, hour?: string, pole?) {
+        const segment_date = date.split('');
+        const day = segment_date[6] + segment_date[7];
+        const month = segment_date[4] + segment_date[5];
+
+        const segment_hour = hour.split('');
+        const hr = segment_hour[0] + segment_hour[1];
+        const min = segment_hour[2] + segment_hour[3];
+
+        if (pole === 'north') {
+            const dateNorth = day + '/' + month;
+            const hourNorth = hr + ':' + min;
+            this.datetimeNorth.push([hourNorth, dateNorth]);
+            return;
+        }
     }
 }
